4d029d67ea991f8d79673c62c8d460b0a3baa043
ankan7810 <162693954+ankan7810@users.noreply.github.com>
2025-10-08 00:34:40 +0530

    Update post.routes.js

--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -1,11 +1,12 @@
 import express from "express"
 import isAuth from "../middlewares/isAuth.js"
 import upload from "../middlewares/multer.js"
-import { comment, createPost, getPost, like } from "../controllers/post.Controllers.js"
+import { comment, createPost, deletePost, getPost, like } from "../controllers/post.Controllers.js"
 const postRouter=express.Router()
 
 postRouter.post("/create",isAuth,upload.single("image"),createPost)
 postRouter.get("/getpost",isAuth,getPost)
+postRouter.delete("/delete/:id", isAuth, deletePost)
 postRouter.get("/like/:id",isAuth,like)
 postRouter.post("/comment/:id",isAuth,comment)
 
